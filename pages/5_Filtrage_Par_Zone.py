294efc8f584eeccfecb3e787fcdfb078af2b682f
elqadiri <153100711+elqadiri@users.noreply.github.com>
2024-10-23 13:55:43 +0100

    Update 5_Filtrage_Par_Zone.py

--- pages/5_Filtrage_Par_Zone.py
+++ pages/5_Filtrage_Par_Zone.py
@@ -3,7 +3,7 @@ import pandas as pd
 import plotly.express as px
 from read_file import read_excel_file
 
-st.set_page_config(page_title="filtrage par zone")
+st.set_page_config(page_title="filtering by area")
 
 @st.cache_data
 def load_data(file_path):
@@ -15,14 +15,14 @@ load_css()
 file_path = 'Data_visual_clean.xlsx'
 
 def main():
-    st.title('Analyse des Biens Immobiliers par Zone')
+    st.title('Analysis of Real Estate by Area')
 
     df = load_data(file_path)
 
     grouped_df = df.groupby(['Zone', 'Action commerciale']).size().reset_index(name='Nombre')
 
     zone_options = df['Zone'].unique()
-    selected_zone = st.selectbox('Sélectionnez une zone:', zone_options)
+    selected_zone = st.selectbox('Select an area:', zone_options)
 
     filtered_data = grouped_df[grouped_df['Zone'] == selected_zone]
 
@@ -31,15 +31,15 @@ def main():
         st.write(filtered_data)
 
         fig = px.bar(filtered_data, x='Action commerciale', y='Nombre', color='Action commerciale',
-                     title=f'Nombre de biens par action commerciale pour {selected_zone}',
+                     title=f'Number of goods per commercial action for {selected_zone}',
                      labels={'Nombre': 'Nombre de biens', 'Action commerciale': 'Type d\'action'})
         st.plotly_chart(fig)
 
         detailed_data = df[df['Zone'] == selected_zone]
-        st.write("Détails des biens dans la zone sélectionnée:")
+        st.write("Details of properties in selected area:")
         st.dataframe(detailed_data) 
     else:
-        st.error("Aucune donnée disponible pour la zone sélectionnée.")
+        st.error("No data available for selected area.")
 
 if __name__ == '__main__':
     main()

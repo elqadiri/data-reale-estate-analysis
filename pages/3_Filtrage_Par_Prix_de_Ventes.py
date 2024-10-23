04295747de89aa035d461ecf9781539f2792cfaa
elqadiri <153100711+elqadiri@users.noreply.github.com>
2024-10-23 13:52:10 +0100

    Update 3_Filtrage_Par_Prix_de_Ventes.py

--- pages/3_Filtrage_Par_Prix_de_Ventes.py
+++ pages/3_Filtrage_Par_Prix_de_Ventes.py
@@ -3,7 +3,7 @@ import pandas as pd
 import plotly.express as px
 from read_file import read_excel_file
 
-st.set_page_config(page_title="filtrage par prix de ventes")
+st.set_page_config(page_title="filtering by sales price")
 @st.cache_data
 def load_data(file_path):
     return read_excel_file(file_path)
@@ -11,7 +11,7 @@ def load_data(file_path):
 from css import load_css
 load_css()
 
-st.title('CapAlliance Data Analysis')
+st.title('Tanger Real Estate Data Analysis')
 
 file_path = 'Data_visual_clean.xlsx'
 
@@ -21,11 +21,11 @@ if 'Zone' in df.columns and 'Surface' in df.columns and 'Prix de vente' in df.co
     selected_zone = st.selectbox('Sélectionnez une zone:', unique_zones)
 
     min_surface, max_surface = int(df['Surface'].min()), int(df['Surface'].max())
-    surface_threshold = st.slider('Filtrer les biens avec une surface inferieur à (m²):', 
+    surface_threshold = st.slider('Filter properties with a surface area less than (m²):', 
                                   min_value=min_surface, max_value=max_surface, 
                                   value=min_surface, key='surface_sale')
     min_price, max_price = int(df['Prix de vente'].min()), int(df['Prix de vente'].max())
-    price_threshold = st.slider('Filtrer les biens avec un prix de vente inferieur à (DH):', 
+    price_threshold = st.slider('Filter properties with a sale price lower than (DH):', 
                                 min_value=min_price, max_value=max_price, 
                                 value=min_price, key='price_sale')
 
@@ -33,16 +33,16 @@ if 'Zone' in df.columns and 'Surface' in df.columns and 'Prix de vente' in df.co
                        (df['Surface'] < surface_threshold) & 
                        (df['Prix de vente'] < price_threshold)]
     
-    st.write(f"Nombre de biens après filtrage: {len(filtered_data)}")
-    if st.checkbox('Afficher les données filtrées'):
+    st.write(f"Number of properties after filtering: {len(filtered_data)}")
+    if st.checkbox('Show filtered data'):
         st.write(filtered_data)
 
-    if st.button('Visualiser la distribution des prix de vente filtrés'):
+    if st.button('View the distribution of filtered sales prices'):
         fig = px.histogram(filtered_data, x='Prix de vente', 
-                           title='Distribution des Prix de Vente Après Filtrage',
-                           labels={'Prix de vente': 'Prix (DH)'},
+                           title='Distribution of Sales Prices After Filtering',
+                           labels={'Selling price': 'Prix (DH)'},
                            color_discrete_sequence=['#636EFA'])
         st.plotly_chart(fig)
 else:
     missing_cols = [col for col in ['Zone', 'Surface', 'Prix de vente'] if col not in df.columns]
-    st.error(f"Les colonnes nécessaires ne sont pas présentes dans le fichier: {', '.join(missing_cols)}")
+    st.error(f"The necessary columns are not present in the file: {', '.join(missing_cols)}")
